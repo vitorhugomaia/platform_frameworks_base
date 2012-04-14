74e21bcc83fadb077cf61de6bc7410e6ea509f72
StevenHarperUK <StevenHarperUK@gmail.com>
2012-04-14 14:22:11 +0100

    Feature : Volume adjust sound preference 1/2
    Adjusting the volume makes a nice "boing" sound
    Personally I find this annoying
    This patch makes this a preference in
    Settings | Sound
        Volume adjust sound
            Play sound when adjusting volume with hard keys
    
    The Checkbox is defaulted to ON (1 in defaults.xml)
    
    Look for 2/2 for the Settings, Sound Panel Change
    
    Change-Id: Iad89a0c49288094e21824aa9c9e1bf98c29cfec3
    
    Conflicts:
    	packages/SettingsProvider/res/values/defaults.xml
    
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1463,6 +1463,12 @@ public final class Settings {
          */
         public static final String MODE_VOLUME_OVERLAY = "mode_volume_overlay";
 
+        /** Volume Adjust Sounds Enable, This is the noise made when using volume hard buttons
+        * Defaults to 1 - sounds enabled
+        * @hide
+        */
+        public static final String VOLUME_ADJUST_SOUNDS_ENABLED = "volume_adjust_sounds_enabled";
+
         /**
          * Ringer mode. This is used internally, changing this value will not
          * change the ringer mode. See AudioManager.
